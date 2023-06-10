a85a3c4ce020b6a2b5fcb98f0b33d9780c40417a
Temo Khutsishvili <tkhut18@freeuni.edu.ge>
2023-06-10 16:30:54 +0400

    Initial commit

---
+++ src/main/java/ge/temojudo/digitalauction/DigitalAuctionApplication.java
@@ -0,0 +1,14 @@
+package ge.temojudo.digitalauction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class DigitalAuctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalAuctionApplication.class, args);
+	}
+
+}
